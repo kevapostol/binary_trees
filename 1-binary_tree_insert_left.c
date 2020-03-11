cd65950cfe45982c20ba14bec128d1b4f51f0fef
ariana124 <ariana.bibiano@gmail.com>
2020-03-11 21:46:04 +0000

    removed memory allocation

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,9 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
-		return (NULL);
+	new = binary_tree_node(parent, value);
 
 	if (parent == NULL)
 		return (NULL);
