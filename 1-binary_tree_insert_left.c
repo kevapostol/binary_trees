d8a217995bf22e0f2f692c155414fc3fa9df9be2
ariana124 <ariana.bibiano@gmail.com>
2020-03-11 21:55:22 +0000

    removed redundant code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,9 +17,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new = binary_tree_node(parent, value);
 
-	new->parent = parent;
-	new->n = value;
-
 	if (parent->left)
 	{
 		new->left = parent->left;
