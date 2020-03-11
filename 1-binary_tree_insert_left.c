ccedf7ae1389a6a64eaf11e92b34e2c0f5980a30
ariana124 <ariana.bibiano@gmail.com>
2020-03-11 21:27:26 +0000

    created seperate malloc check

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,8 +13,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
 
-	if (parent == NULL || new == NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	new->parent = parent;
